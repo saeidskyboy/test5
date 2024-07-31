e19f1fd1b1f01567372276fdb3b062a965ba484d
saeid <saeid@seznam.cz>
2024-07-31 21:51:05 +0200

    clean created resources3

--- main.tf
+++ main.tf
@@ -19,19 +19,19 @@ provider "google" {
   project = "cloud-2255"
 }
 
-module "vpc" {
-  source            = "./vpc"
-  vpc_name          = "my-vpc"
-  subnet_name       = "my-subnet"
-  subnet_ip_range   = "10.0.1.0/25"
-  subnet_region     = "us-central1"  
-}
+# module "vpc" {
+#   source            = "./vpc"
+#   vpc_name          = "my-vpc"
+#   subnet_name       = "my-subnet"
+#   subnet_ip_range   = "10.0.1.0/25"
+#   subnet_region     = "us-central1"  
+# }
 
-module "vm" {
-  source          = "./vm"
-  instance_name   = "my-vm"
-  machine_type    = "e2-micro"  
-  zone            = "us-central1-c"
-  image           = "debian-cloud/debian-11"
-  subnet_id       = module.vpc.subnet_id
-}
+# module "vm" {
+#   source          = "./vm"
+#   instance_name   = "my-vm"
+#   machine_type    = "e2-micro"  
+#   zone            = "us-central1-c"
+#   image           = "debian-cloud/debian-11"
+#   subnet_id       = module.vpc.subnet_id
+# }
--- vm/main.tf
+++ vm/main.tf
@@ -1,15 +1,15 @@
-resource "google_compute_instance" "vm_instance" {
-  # name         = var.instance_name
-  # machine_type = var.machine_type
-  # zone         = var.zone
+# resource "google_compute_instance" "vm_instance" {
+#   # name         = var.instance_name
+#   # machine_type = var.machine_type
+#   # zone         = var.zone
 
-  # boot_disk {
-  #   initialize_params {
-  #     image = var.image
-  #   }
-  # }
+#   # boot_disk {
+#   #   initialize_params {
+#   #     image = var.image
+#   #   }
+#   # }
 
-  # network_interface {
-  #   subnetwork = var.subnet_id
-  # }
-}
+#   # network_interface {
+#   #   subnetwork = var.subnet_id
+#   # }
+# }
