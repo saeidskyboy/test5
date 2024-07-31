5c7bacb183b629e5329045dc12b07557244bb650
saeid <saeid@seznam.cz>
2024-07-31 21:49:54 +0200

    clean created resources2

--- vm/main.tf
+++ vm/main.tf
@@ -1,16 +1,15 @@
 resource "google_compute_instance" "vm_instance" {
-  count = 0
-  name         = var.instance_name
-  machine_type = var.machine_type
-  zone         = var.zone
+  # name         = var.instance_name
+  # machine_type = var.machine_type
+  # zone         = var.zone
 
-  boot_disk {
-    initialize_params {
-      image = var.image
-    }
-  }
+  # boot_disk {
+  #   initialize_params {
+  #     image = var.image
+  #   }
+  # }
 
-  network_interface {
-    subnetwork = var.subnet_id
-  }
+  # network_interface {
+  #   subnetwork = var.subnet_id
+  # }
 }
--- vpc/main.tf
+++ vpc/main.tf
@@ -1,15 +1,13 @@
 resource "google_compute_network" "vpc_network" {
-  count = 0
-  name                    = var.vpc_name
-  auto_create_subnetworks = false 
+  # name                    = var.vpc_name
+  # auto_create_subnetworks = false 
 }
 
 resource "google_compute_subnetwork" "vpc_subnetwork" {
-  count = 0
-  name          = var.subnet_name
-  ip_cidr_range = var.subnet_ip_range
-  region        = var.subnet_region
-  network       = google_compute_network.vpc_network.id
+  # name          = var.subnet_name
+  # ip_cidr_range = var.subnet_ip_range
+  # region        = var.subnet_region
+  # network       = google_compute_network.vpc_network.id
 }
 
 output "vpc_id" {
