dd88c099d78d1b2ff79e8b80986cd7476c73c567
saeid <saeid@seznam.cz>
2024-07-31 21:05:59 +0200

    edit for 2nd test

--- main.tf
+++ main.tf
@@ -21,17 +21,17 @@ provider "google" {
 
 module "vpc" {
   source            = "./vpc"
-  vpc_name          = "my-custom-vpc"
+  vpc_name          = "my-vpc"
   subnet_name       = "my-subnet"
-  subnet_ip_range   = "10.0.1.0/24"
+  subnet_ip_range   = "10.0.1.0/25"
   subnet_region     = "us-central1"  
 }
 
 module "vm" {
   source          = "./vm"
-  instance_name   = "my-vm-instance"
+  instance_name   = "my-vm"
   machine_type    = "e2-micro"  
-  zone            = "us-central1-a"
+  zone            = "us-central1-c"
   image           = "debian-cloud/debian-11"
   subnet_id       = module.vpc.subnet_id
 }
